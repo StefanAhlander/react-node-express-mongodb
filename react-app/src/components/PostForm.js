fbd19ce018f2796323908151c4403f7d9f87db0a
stefan Åhlander <cs.ahlander@gmail.com>
2020-08-20 05:43:59 +0200

    use IFEE to allow async/await in useEffect

--- react-app/src/components/PostForm.js
+++ react-app/src/components/PostForm.js
@@ -10,7 +10,7 @@ export default function PostForm() {
   const [reload, triggerReload] = useState(0);
 
   useEffect(() => {
-    const getPosts = async () => {
+    (async () => {
       try {
         const response = await axios
           .get('http://localhost:5000/posts');
@@ -19,8 +19,7 @@ export default function PostForm() {
       } catch (err) {
         console.log(err);
       }
-    };
-    getPosts();
+    })();
   }, [reload]);
 
   const handleSubmit = async (evt) => {
